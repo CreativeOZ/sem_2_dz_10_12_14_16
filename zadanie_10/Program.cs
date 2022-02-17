7674e7bcd0bab3dc3b12af7caa2e4b066bcc4859
CreativeOZ <zagriaoleg@gmail.com>
2022-02-17 16:22:52 +0300

    Показать вторую цифру трёхзначного числа.

---
+++ zadanie_10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать вторую цифру трёхзначного числа.
+
+Console.Clear();
+
+int TwoNumber(int Number)
+{
+    int result = Number % 11;
+    return result;
+}
+Console.Write("Ввести трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Вторая цифра: " + TwoNumber(num));
