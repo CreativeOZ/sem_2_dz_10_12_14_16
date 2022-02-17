430a7042f466502d7e7716fb792708c79ae429b7
CreativeOZ <zagriaoleg@gmail.com>
2022-02-17 16:34:16 +0300

    Удалить вторую цифру трёхзначного числа.

---
+++ zadanie_12/Program.cs
@@ -0,0 +1,6 @@
+﻿// Удалить вторую цифру трёхзначного числа.
+Console.Clear();
+
+Console.Write("Ввести число: ");
+string A = Console.ReadLine();
+Console.WriteLine("Первая и третья цифра: " + A[0] + A[2]);
